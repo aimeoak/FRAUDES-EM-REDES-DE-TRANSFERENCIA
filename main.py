3bf9d7f825f11377984a9c9b806688a157b5db73
aimeoak <aime.guimar@gmail.com>
2026-05-21 01:21:44 -0300

    Adicionando ontologia

--- main.py
+++ main.py
@@ -1,26 +1,53 @@
 from leitor_csv import Leitor_csv
 from visualizador import Visualizador
-class gerenciador:
+from ontologia import GerenciadorOntologia
 
 
 
-    if __name__ == "__main__":
-        #Aqui eu tava só testando se tava as funções ok, mas coloca aqui alguns testes de nós suspeitos 
-        l = Leitor_csv()
-        v = Visualizador()
-        g = l.carregar("lista_transacoes.csv")
-        v.desenhar_grafo(g)
-        x = g.lista_transacoes_recebidas("G1")
-        for t in x:
-            print(t)
-        y = g.lista_transacoes_enviadas("G1")
-        for t in y:
-            print(t)
-        print(g.grau("G1"))
-        print(g.grau_entrada("G1"))
-        print(g.grau_saida("G1"))
-        #testes das novas funções
-        print(g.total_valor_enviado("G1"))
-        print(g.total_valor_recebido("G1"))
-        print(g.existe_caminho("G1","B3"))
-        print(g.detectar_ciclo())
+
+if __name__ == "__main__":
+    
+    l = Leitor_csv()
+    g = l.carregar("lista_transacoes.csv") 
+    v = Visualizador()
+    print(f"Total de contas monitoradas: {len(g.contas)}")
+    print(f"Total de transações registradas: {len(g.transacoes)}")
+
+    # 2. Análise Estrutural (Grafo)
+    print("ANÁLISE TOPOLÓGICA")
+    tem_ciclo = g.detectar_ciclo()
+    print(f"Alerta Estrutural: Detectado ciclo (dinheiro andando em círculos)? {'[SIM]' if tem_ciclo else '[NÃO]'}")
+    
+    
+    print(f"Existe caminho conectando N1 (alto valor) a A1? {'SIM' if g.existe_caminho('N1', 'A1') else 'NÃO'}")
+
+    
+    print("ONTOLOGIA")
+    onto_manager = GerenciadorOntologia()
+    onto_manager.povoar_com_grafo(g)
+    
+    print("Classificando contas com base no comportamento financeiro")
+    onto_manager.classificar_contas(g)
+
+    #Resultado
+    print("\n=============================================")
+    print("      RELATÓRIO   ")
+    print("=============================================")
+    
+    
+    laranjas = onto_manager.onto.ContaLaranja.instances()
+    fantasmas = onto_manager.onto.ContaFantasma.instances()
+
+    print(f"\n[!] Contas tipo 'Laranja' ({len(laranjas)} detectadas):")
+    if not laranjas: print(" -> Nenhuma")
+    for c in laranjas:
+        print(f" -> {c.name}")
+
+    print(f"\n[!] Contas tipo 'Fantasma' ({len(fantasmas)} detectadas):")
+    if not fantasmas: print(" -> Nenhuma")
+    for c in fantasmas:
+        print(f" -> {c.name}")
+
+    print("\n=============================================")
+
+    v.desenhar_grafo(g)
---
+++ ontologia.py
@@ -0,0 +1,100 @@
+from owlready2 import*
+
+
+class GerenciadorOntologia:
+    def __init__(self):
+        self.onto = get_ontology("http://projetografo.com/financeiro.owl")
+        self.definir_modelo()
+
+    def definir_modelo(self):
+        
+        with self.onto:
+            # Classes
+            class Entidade(Thing): pass
+            class PessoaFisica(Entidade): pass
+            class PessoaJuridica(Entidade): pass
+
+            class Conta(Thing): pass
+            class ContaCorrente(Conta): pass
+            # Conta fantasma é usada para marcar uma conta suspeita/ que faz fraudes
+            class ContaFantasma(Conta): pass
+            class ContaLaranja(Conta): pass
+
+            class Transacao(Thing): pass
+
+            # Relações
+            class pertence_a(ObjectProperty):
+                domain = [Conta]
+                range = [Entidade]
+            class origina_transacao(ObjectProperty): 
+                domain = [Conta]
+                range = [Transacao]
+                
+            class recebe_transacao(ObjectProperty):
+                domain = [Transacao]
+                range = [Conta]
+
+            
+            class possui_valor(DataProperty):
+                domain = [Transacao]
+                range = [float]
+    
+    def povoar_com_grafo(self, grafo):
+        #Transforama dados do grafo em individuos da ontologia
+        with self.onto:
+            dicionario_contas = {}
+            #cria individuos classe conta
+            for conta_id in grafo.contas:
+                nova_conta = self.onto.ContaCorrente(conta_id)
+                dicionario_contas[conta_id] = nova_conta
+            for i,t in enumerate(grafo.transacoes):
+                # Da o nome da transição
+                nome_transacao = f"Transacao_{t.conta_origem}_para_{t.conta_destino}_{i}"
+                nova_transacao = self.onto.Transacao(nome_transacao)
+
+                # Adiciona o valor
+                nova_transacao.possui_valor = [float(t.valor)]
+                
+                c_origem = dicionario_contas[t.conta_origem]
+                c_destino = dicionario_contas[t.conta_destino]
+
+                # Liga os nós
+                c_origem.origina_transacao.append(nova_transacao)
+                nova_transacao.recebe_transacao.append(c_destino)
+    
+    def classificar_contas(self, grafo):
+        for conta_id in grafo.contas:
+            
+            enviado = grafo.total_valor_enviado(conta_id)
+            recebido = grafo.total_valor_recebido(conta_id)
+            entradas = grafo.grau_entrada(conta_id)
+            saidas = grafo.grau_saida(conta_id)
+
+            
+            conta_owl = self.onto.ContaCorrente(conta_id) 
+
+            #CONTA LARANJA
+            #Ela recebe de várias fontes, mas envia tudo para apenas um destino
+            if entradas >= 2 and saidas == 1 and recebido > 0:
+                taxa_repasse = enviado / recebido
+                if taxa_repasse >= 0.80:  #Repassou 80% ou mais do que recebeu
+                    conta_owl.is_a.append(self.onto.ContaLaranja)
+
+            #CONTA FANTASMA
+            #Alto volume de dinheiro entrando e saindo, saldo final mascarado
+            if recebido >= 2000 and enviado >= 2000:
+                margem_diferenca = abs(recebido - enviado)
+                # Se a diferença entre entrada e saída for menor que 10% do total
+                if margem_diferenca <= (recebido * 0.1): 
+                    conta_owl.is_a.append(self.onto.ContaFantasma)
+
+       
+
+    def salvar_arquivo(self, nome_arquivo="minha_ontologia.owl"):
+        self.onto.save(file=nome_arquivo, format="rdfxml")
+        print(f"Ontologia salva com sucesso em {nome_arquivo}")
+
+
+
+        
+    
